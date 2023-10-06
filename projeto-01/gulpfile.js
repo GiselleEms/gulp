704a0363ddf65ee1a55a64e0179ffa67fdf9262d
Giselle Emydio <giselleemydiodesign@gmail.com>
2023-10-06 18:16:30 -0300

    arquivos corrigidos exercicio gulp

--- projeto-01/gulpfile.js
+++ projeto-01/gulpfile.js
@@ -3,17 +3,16 @@ const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
-const image = require('gulp-image') 
+const image = require('gulp-imagemin')
 
 function tarefasCSS(cb) {
 
     return gulp.src([
             './node_modules/bootstrap/dist/css/bootstrap.css',
-            './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+            './node_modules/font-awesome/css/font-awesome.css',
             './vendor/owl/css/owl.css', 
             './src/css/style.css'
         ])
-        .pipe(stripCss())                   // remove comentários
         .pipe(concat('styles.css'))         // mescla arquivos
         .pipe(cssmin())                     // minifica css
         .pipe(rename({ suffix: '.min'}))    // styles.min.css
@@ -29,7 +28,6 @@ function tarefasJS(){
             './vendor/owl/js/owl.js',
             './src/js/custom.js'
         ])
-        .pipe(stripJs())                    // remove comentários
         .pipe(concat('scripts.js'))         // mescla arquivos
         .pipe(uglify())                     // minifica js
         .pipe(rename({ suffix: '.min'}))    // scripts.min.js
